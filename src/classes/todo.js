74886b9a04e335aad04661e2fc22d48ccb9bb8d2
Edmond <edmondchu10@hotmail.com>
2025-01-01 20:46:05 +0000

    feat: Add initial constructors for Todo and Project class

---
+++ src/classes/todo.js
@@ -0,0 +1,9 @@
+export class Todo {
+    constructor(title,description,dueDate,priority,completed) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = completed;
+    }
+}
