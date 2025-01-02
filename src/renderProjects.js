fabb4a8a49024b008dd03a93c23ffe1829b7a0c5
Edmond <edmondchu10@hotmail.com>
2025-01-02 22:57:28 +0000

    feat: Add functions to display the projects
    project cards have delete buttons
    delete buttons have event listener added using event delegation

--- src/renderProjects.js
+++ src/renderProjects.js
@@ -3,6 +3,7 @@ import { Project } from './classes/Project.js'
 
 export const renderProjects = (() => {
     const projectManager = new ProjectManager();
+    const projectDisplay = document.querySelector('.project-display');
     createListeners();
 
     function createListeners() {
@@ -18,18 +19,62 @@ export const renderProjects = (() => {
 
         submitProject.addEventListener('click', ()=> {
             addProject(nameInput.value);
-            nameInput.innerHTML = '';
+            nameInput.value = '';
             projectModal.close();
+            displayProjects();
         });
 
         cancelModal.addEventListener('click', ()=>{
             projectModal.close();
         });
+
+        projectDisplay.addEventListener('click',(event) => {
+            const target = event.target;
+            if (target.classList.contains('delete-project')) {
+                const deleteIndex = target.getAttribute('id');
+                deleteProject(deleteIndex);
+                displayProjects();
+            }
+        })
     }
 
     function addProject(name) {
         const project = new Project(name);
         projectManager.addProject(project);
+    }
+    
+    function deleteProject(index) {
+        projectManager.removeProject(index);
+    }
+
+    function displayProjects() {
+        clearProjects();
+        projectManager.allProjects.forEach((project,index)=>{
+            const card = createProjectCard(project,index);
+            projectDisplay.append(card);
+        })
+    }
+
+    function createProjectCard(project,index) {
+        const card = document.createElement('div');
+        const projectTitle = document.createElement('h5');
+        const deleteProject = document.createElement('button');
+
+        card.setAttribute('data-index',index);
+        card.classList.add('project-card');
+        projectTitle.classList.add('project-title');
+        deleteProject.classList.add('delete-project');
+        deleteProject.setAttribute('id',index);
+
+        projectTitle.textContent = project.name;
+        deleteProject.textContent = 'Delete';
+
+        card.append(projectTitle,deleteProject);
+        return card;
+    }
+
+    function clearProjects() {
+        projectDisplay.innerHTML = '';
     }
     
 })();
